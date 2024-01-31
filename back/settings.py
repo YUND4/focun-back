c2fb0e5ce8fa89966bf30e720a8cdb5536696ea1
Santiago Yunda <syundarivera@gmail.com>
2024-01-30 22:45:59 -0500

    add tests

--- back/settings.py
+++ back/settings.py
@@ -18,7 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_yasg'
+    'drf_yasg',
+    'health'
 ]
 
 MIDDLEWARE = [
--- back/urls.py
+++ back/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from back.router import router
 from back.swagger import urlpatterns as docs_urlpatterns
+from health.urls import urlpatterns as health_urlpatterns
 
 urlpatterns = [
+    path('health/', include((health_urlpatterns, 'health'))),
     path('docs/', include((docs_urlpatterns, 'docs'))),
     path('api/', include(router.urls)),
 ]
